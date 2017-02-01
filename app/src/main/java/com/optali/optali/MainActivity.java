98ee89ebf6c69e51e00f9092b3a37e2de1bbbe17
alex67 <huet.alexandre@gmail.com>
2017-02-01 21:15:26 +0100

    Ajout de l'activité "ajout"

--- app/src/main/java/com/optali/optali/MainActivity.java
+++ app/src/main/java/com/optali/optali/MainActivity.java
@@ -1,7 +1,10 @@
 package com.optali.optali;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button1 = (Button) findViewById(R.id.ajoutPage);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),AjoutActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
+
+
