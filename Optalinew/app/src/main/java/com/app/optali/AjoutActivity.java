8fe47ac54f23e16d457972151fbadc04cdd85410
FrancoisLefevre12 <lefevre.francois12@gmail.com>
2017-02-08 15:00:03 +0100

    Ajout des fichiers globaux

---
+++ Optalinew/app/src/main/java/com/app/optali/AjoutActivity.java
@@ -0,0 +1,20 @@
+package com.app.optali;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by alex67 on 01/02/17.
+ */
+
+public class AjoutActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ajout);
+    }
+
+
+}
