86189c19f7056dbfa0673ecf71236af797d384b0
ahuet67 <alexandre.huet@polytech-lille.net>
2017-02-27 18:22:04 +0100

    Insertion dans la base de donnée et premières requêtes dans la base.

--- serveur/post.php
+++ serveur/post.php
@@ -32,7 +32,6 @@ else {
 
 
 
-
 //----------------------------------------------------\\
 // De même pour la Date
 $sql="SELECT * FROM Date where Date='".$Date."';";
@@ -40,12 +39,12 @@ $result= mysql_query($con,$sql);
 
 // Si la date existe déjà, on ajoute +1 à NbDate
 if(mysqli_num_rows($result)>0)
-{	
+{
 	$sql="UPDATE Date SET NbDate = NbDate+1 WHERE Date='".$Date."';";
 	$result= mysql_query($con,$sql);
 }
 else {
-	$sql="insert into Date values('".$Date."');";
+	$sql="insert into Date (Date) values('".$Date."');";
 	$result= mysql_query($con,$sql);
 }
 
@@ -54,18 +53,18 @@ else {
 //-----------------------------------------------------\\
 // On récupère les clefs primaires pour rajouter une ligne dans la table Unity
 $sql="SELECT IdDate FROM Date where Date ='".$Date."';";
-$idDate= mysql_query($con,$sql);
+$IdDate= mysql_query($con,$sql);
 
 $sql="SELECT IdProduct FROM Product where Product='".$Product."';";
-$idProduct= mysql_query($con,$sql);
+$IdProduct= mysql_query($con,$sql);
 
 // On regarde s'il n'existe déjà pas un produit avec la même date de péremption
-$sql="SELECT * FROM Unity where (idDate='".$idDate."' AND idProduct='"$idProduct"');";
+$sql="SELECT * FROM Unity where (IdDate='".$IdDate."' AND IdProduct='"$IdProduct"');";
 $result= mysql_query($con,$sql);
 
 if(mysqli_num_rows($result)>0)
 {
-	$sql="UPDATE Unity SET NbUnity = NbUnity+1 WHERE (idDate='".$idDate."' AND idProduct='"$idProduct"');";
+	$sql="UPDATE Unity SET NbUnity = NbUnity+1 WHERE (IdDate='".$IdDate."' AND IdProduct='"$IdProduct"');";
 	$result= mysql_query($con,$sql);
 }
 else
