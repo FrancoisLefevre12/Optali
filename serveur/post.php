61c03a11ddee48d57ceceffdad5a560664828c7f
ahuet67 <alexandre.huet@polytech-lille.net>
2017-03-06 18:29:02 +0100

    Finition de la page post

--- serveur/post.php
+++ serveur/post.php
@@ -5,15 +5,15 @@
 require "init.php";
 echo '<br>Page post!!<br>';
 
-$Product=$_Post["Product"];
-$Date=$_Post["Date"];
+$Product=$_POST['Product'];
+$Date=$_POST['Date'];
 
 
 
 // ---------------------------------------------------- \\
 // Pour on regarde si le produit existe déjà
 $sql="SELECT * FROM Product where Product='".$Product."';";
-$result= mysql_query($con,$sql);
+$result= mysqli_query($con,$sql);
 
 // Si le produit existe déjà, on ajoute +1 au produit
 if(mysqli_num_rows($result)>0)
@@ -21,14 +21,14 @@ if(mysqli_num_rows($result)>0)
 
 	//On augmente le nombre de ce produit dans le frigo
 	$sql="UPDATE Product SET NbProdIn = NbProdIn+1 WHERE Product='".$Product."';";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 
 	$sql="UPDATE Product SET NbProdBought = NbProdBought+1 WHERE Product='".$Product."';";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 }
 else {
 	$sql="insert into Product (Product, NbProdIn, NbProdBought) values('".$Product."','1','1');";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 }
 
 
@@ -36,17 +36,17 @@ else {
 //----------------------------------------------------\\
 // De même pour la Date
 $sql="SELECT * FROM Date where Date='".$Date."';";
-$result= mysql_query($con,$sql);
+$result= mysqli_query($con,$sql);
 
 // Si la date existe déjà, on ajoute +1 à NbDate
 if(mysqli_num_rows($result)>0)
 {
 	$sql="UPDATE Date SET NbDate = NbDate+1 WHERE Date='".$Date."';";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 }
 else {
 	$sql="insert into Date (Date) values('".$Date."');";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 }
 
 
@@ -54,24 +54,28 @@ else {
 //-----------------------------------------------------\\
 // On récupère les clefs primaires pour rajouter une ligne dans la table Unity
 $sql="SELECT IdDate FROM Date where Date ='".$Date."';";
-$IdDate= mysql_query($con,$sql);
+$result= mysqli_query($con,$sql);
+$row= mysqli_fetch_assoc($result);
+$IdDate= $row['IdDate'];
 
 $sql="SELECT IdProduct FROM Product where Product='".$Product."';";
-$IdProduct= mysql_query($con,$sql);
+$result= mysqli_query($con,$sql);
+$row= mysqli_fetch_assoc($result);
+$IdProduct= $row['IdProduct'];
 
 // On regarde s'il n'existe déjà pas un produit avec la même date de péremption
 $sql="SELECT * FROM Unity where (IdDate='".$IdDate."' AND IdProduct='".$IdProduct."');";
-$result= mysql_query($con,$sql);
+$result= mysqli_query($con,$sql);
 
 if(mysqli_num_rows($result)>0)
 {
 	$sql="UPDATE Unity SET NbUnity = NbUnity+1 WHERE (IdDate='".$IdDate."' AND IdProduct='".$IdProduct."');";
-	$result= mysql_query($con,$sql);
+	$result= mysqli_query($con,$sql);
 }
 else
 {
-	$sql="insert into Unity (idDate,idProduct,NbUnity) values('".$idDate."','".$idProduct."','1');";
-	$result= mysql_query($con,$sql);
+	$sql="insert into Unity (idDate,idProduct,NbUnity) values('".$IdDate."','".$IdProduct."','1');";
+	$result= mysqli_query($con,$sql);
 }
 
 
