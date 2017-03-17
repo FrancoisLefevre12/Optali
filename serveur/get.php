46afa2705eeeda7ed94e0896494b0f6a1d3b1c01
ahuet67 <alexandre.huet@polytech-lille.net>
2017-03-17 07:22:51 +0100

    correction de la page get.php

--- serveur/get.php
+++ serveur/get.php
@@ -3,35 +3,21 @@
 <?php
 
 require "init.php";
-echo '<br>Page get!!<br>';
 
-// OrderBy = 0 si par nom, 1 si par date, 2 si par Nb dans le frigo, 3 si par Nb en tout
-$OrderBy=$_POST['OrderBy'];
-$Search=$_POST['Search'];
-
-if($OrderBy==0){
-$sql="select Product.Product,Date.Date,Product.NbProdIn,Product.NbProdBought from Product left join (Unity,Date) ON (Unity.IdProduct=Product.IdProduct AND Unity.IdDate=Date.IdDate) where Product.Product='".$Search."%' Order By Product.Product;";
+$sql="select Product.Product,Date.Date,Product.NbProdIn,Product.NbProdBought from Product left join (Unity,Date) ON (Unity.IdProduct=Product.IdProduct AND Unity.IdDate=Date.IdDate);";
 $result= mysqli_query($con,$sql);
+$response=array();
+
+while($row=mysqli_fetch_array($result)){
+
+array_push($response,array("sProduct"=>$row["Product"],"sDate"=>$row["Date"],"sStock"=>$row["NbProdIn"],"sHisto"=>$row["NbProdBought"]));
+
 }
 
-if($OrderBy==1){
-$sql="select Product.Product,Date.Date,Product.NbProdIn,Product.NbProdBought from Product left join (Unity,Date) ON (Unity.IdProduct=Product.IdProduct AND Unity.IdDate=Date.IdDate) where Product.Product='".$Search."%' Order By Date.Date;";
-$result= mysqli_query($con,$sql);
-}
+echo json_encode($response);
 
-if($OrderBy==2){
-$sql="select Product.Product,Date.Date,Product.NbProdIn,Product.NbProdBought from Product left join (Unity,Date) ON (Unity.IdProduct=Product.IdProduct AND Unity.IdDate=Date.IdDate) where Product.Product='".$Search."%' Order By Product.NbProdIn;";
-$result= mysqli_query($con,$sql);
-}
+echo '<br>Requete bien envoyée!!';
 
-if($OrderBy==3){
-$sql="select Product.Product,Date.Date,Product.NbProdIn,Product.NbProdBought from Product left join (Unity,Date) ON (Unity.IdProduct=Product.IdProduct AND Unity.IdDate=Date.IdDate) where Product.Product='".$Search."%' Order By Product.NbProdBought;";
-$result= mysqli_query($con,$sql);
-}
-
-while($row= mysqli_fetch_assoc($result)){
-$array[]=$row;
-}
-echo json_encode($array);
+mysqli_close($con);
 
 ?>
