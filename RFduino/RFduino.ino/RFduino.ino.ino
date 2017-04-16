d1878b1d99bdb52d62f1fc0d88109ecec7915877
FrancoisLefevre12 <lefevre.francois12@gmail.com>
2017-04-16 16:56:52 +0200

    version presque finale du programme RFduino

--- RFduino/RFduino.ino/RFduino.ino.ino
+++ RFduino/RFduino.ino/RFduino.ino.ino
@@ -4,8 +4,8 @@
 
 #define SIZE 50
 
-#define PIN_LED2 1
-#define PIN_LED3 6
+#define PERIM_LED 1
+#define EMPTY_LED 6
 #define PIN_BUZZER 2
 #define PIN_SDA 5
 #define PIN_SCL 4
@@ -21,8 +21,10 @@
 #define OUT_Z_MSB 0x05
 
 int AccX,AccY,AccZ;
+int init_X,init_Y,init_Z;
 int status;
-int flag = false;
+int flag_connect = false;
+int flag_open = false;
 
 
 
@@ -33,9 +35,8 @@ void setup() {
   Serial.println("Serial rate set to 9600 baud");
 
   //Initialisation des pins
-  pinMode(PIN_LED2,OUTPUT); 
-  pinMode(PIN_LED3,OUTPUT); 
-  pinMode(PIN_INT1,INPUT);  
+  pinMode(PERIM_LED,OUTPUT); 
+  pinMode(EMPTY_LED,OUTPUT);  
   pinMode(PIN_BUZZER,OUTPUT);  
 
   init_BLE(); 
@@ -46,10 +47,27 @@ void setup() {
 void loop() 
 {
   // switch to lower power mode
-  RFduino_ULPDelay(INFINITE);
-  
-  //Buzzer();
+  RFduino_ULPDelay(1000);
+
+  //Permet d'envoyer une notification de connexion à l'appli
+  if (flag_connect) {
+    send_BLE("On",strlen("On"));
+  }
+  else {
+    send_BLE("Off",strlen("Off"));
+  }
+
   //lecture_Accelero();
+
+  //Envoi d'un avertissement d'ouverture du frigo à l'appli
+  if (ouverture(AccX,AccY,AccZ)) {
+    send_BLE("Open",strlen("Open"));
+    Buzzer();
+  }
+  else {
+    send_BLE("Close",strlen("Close"));
+  }
+
 }
 
 
@@ -71,24 +89,27 @@ void init_Accelero()
 
 
 
+//Récupère les valeurs pour chaque axe de l'accéléromètre
 void lecture_Accelero()
 {
   //On choisit le registre sur lequel la lecture commence
   Wire.beginTransmission(ACCEL_WRITE);
   Wire.write(OUT_X_MSB);
   status = Wire.endTransmission(false);
-  if (status == 4) { Serial.println("Mode lecture"); }
+  if (status == 0) { Serial.println("Mode lecture"); }
 
   //On peut maintenant lire les 3 registres X,Y et Z
   Wire.requestFrom(ACCEL_READ,3);
-  
+
+  //Tant que le bus i2C n'est pas vide
   if (Wire.available() <= 3) {
     Serial.print("bonjour");
     AccX = Wire.read();
     AccY = Wire.read();
     AccZ = Wire.read();
   }
-  
+
+  //Affichage des coordonnées
   Serial.print("X = ");
   Serial.print(AccX);
   Serial.print(" Y = ");
@@ -96,21 +117,46 @@ void lecture_Accelero()
   Serial.print(" Z = ");
   Serial.println(AccZ);
 
-  delay(5000); 
+  delay(500); 
 }
 
 
+//Retourne true si la porte du frigo est ouverte, 0 sinon
+boolean ouverture(int AccX,int AccY,int AccZ)
+{
+  //Si les coordonnées changent, la porte a été ouverte
+  if (init_X != AccX or init_Y != AccY or init_Z != AccZ) {
+    flag_open = true;
+  }
 
+  //Si les coordonnées sont identiques à celles enregistrées
+  //juste avant l'ouverture de la porte, elle s'est fermée
+  if (init_X == AccX and init_Y == AccY and init_Z == AccZ) {
+    flag_open = false;
+  }
+
+
+  //On récupère toujours les valeurs précédentes lorsque la porte n'est 
+  // pas ouverte pour pouvoir les comparer aux suivantes
+  if (!flag_open) {
+    init_X = AccX;
+    init_Y = AccY;
+    init_Z = AccZ;
+  }
+
+  return flag_open;
+}
+
+
+//Active le buzzer
 void Buzzer()
 {
   digitalWrite(PIN_BUZZER,HIGH);
-  Serial.println(digitalRead(PIN_BUZZER));
   delay(3);
   digitalWrite(PIN_BUZZER,LOW);
-  Serial.println(digitalRead(PIN_BUZZER));
   delay(3);
-  
 }
+
 
 
 void init_BLE()
@@ -126,52 +172,52 @@ void init_BLE()
   
 }
 
-//envoi de donnees
+//Envoi de données
 void send_BLE(const char* data,int len)
 {
   RFduinoBLE.send(data,len);
 }
 
 
-
+//Callback sur connexion bluetooth
 void RFduinoBLE_onConnect()
 {
-  flag = true;
-  send_BLE("On",strlen("On"));
-  delay(20);
+  flag_connect = true;
   Serial.println("Connexion");
 }
 
 
+//Callback sur déconnexion bluetooth
 void RFduinoBLE_onDisconnect()
 {
-  flag = false;
-  send_BLE("Off",strlen("Off"));
-  delay(20);
+  flag_connect = false;
   Serial.println("Deconnexion");
 }
 
 
-
+//Actions à réaliser lors de la réception de données
 void RFduinoBLE_onReceive(char* data,int len)
 {
   if (strstr(data,"coucou") != NULL)
   {  
     Serial.println("Récupération des données effectuée");
     Serial.println("Envoi de la réponse");
-
-    send_BLE("Open",strlen("Open"));
   }
-}
 
-
-
-void envoi_Donnees()
-{
-  if (flag)
-  {
-    //Envoi des coordonnees de l'accelerometre
-    //avec fonction send_BLE
+  if (strstr(data,"Perim") != NULL) {
+    digitalWrite(PERIM_LED,HIGH);
   }
+
+  if (strstr(data,"NotPerim") != NULL) {
+    digitalWrite(PERIM_LED,LOW);
+  }
+
+  if (strstr(data,"Vide") != NULL) {
+    digitalWrite(EMPTY_LED,HIGH);
+  }
+
+  if (strstr(data,"Plein") != NULL) {
+    digitalWrite(EMPTY_LED,LOW);
+  }  
 }
 
