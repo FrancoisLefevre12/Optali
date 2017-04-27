8d01d4657836d88632ef21336745abb032f2b162
ahuet67 <alexandre.huet@polytech-lille.net>
2017-04-27 09:28:31 +0200

    Ajout des listes recettes dans ListeRecette

--- Optalinew/app/src/main/java/com/app/optali/ListeRecette.java
+++ Optalinew/app/src/main/java/com/app/optali/ListeRecette.java
@@ -2,18 +2,33 @@ package com.app.optali;
 
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.ContextMenu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListeRecette extends AppCompatActivity {
 
     public static final String REGISTER_CONSUME_URL = "http://89.80.34.165/optali/listeRecette.php";
+    public static final String[] listString = {"Jean","Pierre","Pascal"};
+    public static final String[] listRecettes = {"Gateau au chocolat","Lasagne","Pizza"};
     private Button bSuppr;
     private EditText eRecherche;
-
+    private ListView listView;
+    private TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +44,54 @@ public class ListeRecette extends AppCompatActivity {
         });
 
         eRecherche = (EditText) findViewById(R.id.recherche_recette);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        tableLayout = (TableLayout) findViewById(R.id.tableRecette);
+
+        for(String s : listRecettes){
+            createRow(s);
+        }
+
+        // Dès qu'une recette est cliquée, on regarde les aliments dont on a besoin...
+        listView = new ListView(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.liste_ingredients,R.id.textItem,listString);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                ViewGroup viewGroup= (ViewGroup) view;
+                                                TextView txt = (TextView) viewGroup.findViewById(R.id.textItem);
+                                                Toast.makeText(ListeRecette.this,txt.getText().toString(),Toast.LENGTH_LONG).show();
+
+                                            }
+                                        });
+
     }
 
-    /*
-    // Mauvais bail parce l'on ne peut pas modifier en menu deroulant en fonction de l'où on appui.
-    public void OnCreateMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
-        menu.setHeaderTitle("Ingrédients");
-        menu.add(0,v.getId(),0,"Youyou1"); //GroupId, itemId, order, title
-        menu.add(0,v.getId(),0,"Youyou2");
+
+    public void showDialogListView(View view){
+        AlertDialog.Builder builder=new AlertDialog.Builder(ListeRecette.this);
+        builder.setCancelable(true);
+        builder.setPositiveButton("Ok",null);
+        builder.setView(listView);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
-    */
+
+    public void createRow(String s1){
+        // On crée une ligne avec les paramètres match_parent
+        final TableRow tableRow = new TableRow(this);
+        tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
+
+        // Création d'un textView du nom
+        final TextView tRecipe = new TextView(this);
+        tRecipe.setText(s1);
+        tRecipe.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
+        tRecipe.setTextSize(TypedValue.COMPLEX_UNIT_SP,18);
+
+        tableRow.addView(tRecipe);
+
+        tableLayout.addView(tableRow);
+    }
 
 
 }
